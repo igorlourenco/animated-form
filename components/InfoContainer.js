2b003002d33c31fd73cd4543089929a0f38518ce
Igor Lourenço <theigorlourenco@gmail.com>
2020-09-18 18:49:19 -0300

    Página Inicial - Ajuste

--- components/InfoContainer.js
+++ components/InfoContainer.js
@@ -1,55 +1,68 @@
 import React from "react";
-import {Box, Flex, Heading, Image, Stack} from "@chakra-ui/core";
+import {Box, Flex, Heading, Icon, Image, Stack} from "@chakra-ui/core";
+import Login from "./sections/Login";
+import {motion} from "framer-motion";
 
 InfoContainer.defaultProps = {
-    title: "Aqui vai alguma coisa",
-    subtitle:
-        "Nesse lugar, vou escrever sobre alguma coisa que possa chamar atenção dos possíveis usuários",
-    image: "",
+    title: "Título Padrão 1",
+    list: ["elemento1", "segundo elemento", "elemento 3 aqui", "mais coisas vêm aqui", "eita lelê"],
+    image: "/images/team-rafiki.svg",
 }
 
-export default function InfoContainer({title, subtitle, image, ...rest}) {
+const mountList = (list) => {
     return (
-        <Flex
-            shadow="2xl"
-            borderRadius="md"
-            backgroundColor="gray.800"
-            align="center"
-            justify={{ base: "center", md: "space-around", xl: "space-between" }}
-            direction={{ base: "column-reverse", md: "row" }}
-            wrap="no-wrap"
-            minH="70vh"
-            px={[1, 1, 8, 8]}
-            mb={16}
-            {...rest}
-        >
-            <Stack
-                spacing={4}
-                w={{ base: "80%", md: "50%" }}
-                align={["center", "center", "flex-start", "flex-start"]}
-            >
-                <Heading
-                    as="h1"
-                    size="xl"
-                    fontWeight="bold"
-                    textAlign={["center", "center", "left", "left"]}
-                >
-                    {title}
-                </Heading>
+        list.map( (list_element) => {
+            return (
                 <Heading
                     as="h2"
                     size="md"
                     opacity="0.8"
                     fontWeight="normal"
                     lineHeight={1.5}
-                    textAlign={["center", "center", "left", "left"]}
+                    textAlign="left"
                 >
-                    {subtitle}
+                    <Icon name="check" size="24px" color="teal.500"/> {"  "} {list_element}
                 </Heading>
+            )
+        } )
+
+    )
+}
+
+export default function InfoContainer({title, list, image, ...rest}) {
+    return (
+        <Flex
+            shadow="md"
+            borderRadius="md"
+            backgroundColor="gray.800"
+            align="center"
+            justify="space-around"
+            direction={{base: "column-reverse", md: "row"}}
+            wrap="no-wrap"
+            width="160vh"
+            p={[3, 3, 8, 8]}
+            mb={[1, 1, 0, 0]}
+            {...rest}
+        >
+            <Image src="/images/team-rafiki.svg" size="35%"/>
+
+            <Stack
+                spacing={4}
+                w={{base: "90%", md: "35%"}}
+                verticalAlign="center"
+                align="flex-start"
+            >
+                {mountList( list )}
             </Stack>
-            <Box w={{ base: "80%", sm: "70%", md: "40%", lg: "40%" }} mb={{ base: 12, md: 0 }} borderRadius="md" backgroundColor="gray.700">
-                <Image src="/images/team-rafiki.svg" size="100%" shadow="2xl" />
-            </Box>
+
+            <motion.div
+                        whileTap={{
+                            scale: 0.95,
+                            transition: {duration: 0.2},
+                        }}
+            >
+            <Login/>
+            </motion.div>
         </Flex>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {Flex, Heading} from "@chakra-ui/core";
+import {Flex} from "@chakra-ui/core";
 import Header from "../components/sections/Header";
 import InfoContainer from "../components/InfoContainer";
-
+import {motion} from "framer-motion";
 export default function Home() {
     return (
         <Flex
@@ -11,7 +11,25 @@ export default function Home() {
             m="0 auto"
         >
             <Header/>
-            <InfoContainer/>
+            <Flex
+                gridGap={16}
+                align="center"
+                justify="center"
+                direction={["column", "column", "column", "row"]}
+                width="100%"
+                backgroundImage="/images/team-rafiki.svg"
+                p={[3, 3, 0, 0]}
+            >
+                <motion.div whileHover={{
+                    scale: 1.05,
+                    transition: {duration: 0.2},
+                }}
+                >
+                    <InfoContainer title="Quais são os Benefícios?"
+                                   list={["elemento1", "segundo elemento sdgsd sdhsdhssdhsd", "elemento 3 aqui", "mais coisas vêm aqui hehehehe", "eita lelê"]}
+                                   image="/images/team-rafiki.svg"/>
+                </motion.div>
+            </Flex>
         </Flex>
     )
 }
